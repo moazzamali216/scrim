26c0eb64ec266fea49511f2f67c4b8596629bb55
moazzamali216 <113228966+moazzamali216@users.noreply.github.com>
2025-09-30 21:18:56 +0500

    main

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,72 @@ function Index() {
 
   return (
     <>
-<h1 className="border border-[red]">Hey</h1>
+      <section className="w-full  mt-[80px] ">
+        <div className="max-w-[986px]  mx-auto poppins  text-center container ">
+          <h1 className="text-[40px] font-[900] text-[#092061]">Subscription</h1>
+          <p className="mt-[14px] poppins text-[#2A2A2A] font-[21px]">Manage your Flight Ping subscription and usage</p>
+          <div className="mt-[66px] flex justify-between ">
+            <div className="flex   w-fit items-center  gap-[10px]">
+              <div className="p-[20px]"><svg width="24" height="24" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.7656 21.5C10.9412 21.804 11.1936 22.0565 11.4977 22.232C11.8017 22.4075 12.1466 22.4999 12.4976 22.4999C12.8487 22.4999 13.1936 22.4075 13.4976 22.232C13.8016 22.0565 14.0541 21.804 14.2296 21.5" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M3.76127 15.826C3.63063 15.9692 3.54442 16.1472 3.51312 16.3385C3.48183 16.5298 3.50679 16.726 3.58498 16.9034C3.66316 17.0807 3.7912 17.2316 3.95352 17.3375C4.11585 17.4434 4.30545 17.4999 4.49927 17.5H20.4993C20.6931 17.5001 20.8827 17.4438 21.0451 17.3381C21.2076 17.2324 21.3358 17.0817 21.4142 16.9045C21.4926 16.7273 21.5178 16.5311 21.4867 16.3398C21.4557 16.1485 21.3697 15.9703 21.2393 15.827C19.9093 14.456 18.4993 12.999 18.4993 8.5C18.4993 6.9087 17.8671 5.38258 16.7419 4.25736C15.6167 3.13214 14.0906 2.5 12.4993 2.5C10.908 2.5 9.38185 3.13214 8.25663 4.25736C7.13141 5.38258 6.49927 6.9087 6.49927 8.5C6.49927 12.999 5.08827 14.456 3.76127 15.826Z" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+              </div>
+              <div className="flex flex-col gap-[3px]"><h5 className="text-[#2A2A2A] text-start text-[21px] font-[700] poppins">Local the commuter</h5>
+                <p className="text-[21px] font-[400] poppins text-start text-[#2A2A2A]">TT $50 billed monthly</p>
+              </div>
+            </div>
+            <div className="flex  w-fit items-center  gap-[10px]">
+              <div className="p-[20px]"><svg width="18" height="20" viewBox="0 0 21 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.5 1.5V5.5M14.5 1.5V5.5M1.5 9.5H19.5M3.5 3.5H17.5C18.6046 3.5 19.5 4.39543 19.5 5.5V19.5C19.5 20.6046 18.6046 21.5 17.5 21.5H3.5C2.39543 21.5 1.5 20.6046 1.5 19.5V5.5C1.5 4.39543 2.39543 3.5 3.5 3.5Z" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+
+              </div>
+              <div className="flex flex-col gap-[3px]"><h5 className="text-[#2A2A2A] text-start text-[21px] font-[700] poppins">Next billing date</h5>
+                <p className="text-[21px] font-[400] poppins text-start text-[#2A2A2A]">August 27, 2025</p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-[26px] px-[24px] pt-[24px] pb-[74px]   ">
+            <div className="flex justify-between">
+              <h5 className="text-[#2A2A2A] text-start text-[21px] font-[700] poppins">Flights check used</h5>   <p className="text-[21px] font-[400] poppins text-start text-[#2A2A2A]"><span>27</span>/ <span>200</span></p>
+            </div>
+
+            <div>
+              <div class="w-full bg-[#EAEFF2] !rounded-[3px] h-[6px] ">
+                <div class="bg-[#51A3FA] my-[6px] h-[6px] !rounded-[3px] w-[30%]"></div>
+              </div>
+            </div>
+            <div className="flex mt-[6px] justify-between">
+              <p className="text-[21px] font-[400] poppins text-start text-[#2A2A2A]">75</p>
+              <p className="text-[21px] font-[400] poppins text-start text-[#2A2A2A]">65%</p>
+              <p className="text-[21px] font-[400] poppins text-start text-[#2A2A2A]">200</p>
+
+            </div>
+            <div className="mt-[14px] flex justify-between my-[24px] mx-[32px]">
+              <h5 className="text-[#2A2A2A] text-start text-[21px] font-[700] poppins">Auto Renewal</h5>
+    <div className="relative ">
+            <label class="inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" class="sr-only peer" />
+<label class="inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" class="sr-only peer" />
+  <div
+    class="relative w-[34px] h-[18px] bg-gray-300 rounded-full peer
+    peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
+    after:content-[''] after:absolute after:top-[1px] after:start-[1px] after:bg-white after:rounded-full after:h-[16px] after:w-[16px] after:transition-all
+    peer-checked:bg-[#22C55D]">
+  </div>
+</label>
+
+</label>
+
+    </div>
+
+
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
